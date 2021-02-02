4e535303da56d61993ea86b69268ae7ef5f7190d
Michael Bargatin <attractadore02@gmail.com>
2021-02-02 11:05:42 +0300

    MSM WIP

--- assets/shaders/src/shadows/shadow_setting_uniforms.glsl
+++ assets/shaders/src/shadows/shadow_setting_uniforms.glsl
@@ -2,10 +2,13 @@
 #define SHADOW_SETTING_UNIFORMS_GLSL
 
 #include "indices.glsl"
+#include "shadow_modes.glsl"
 
 layout(location = B_SHADOW_CASCADE_DEPTH_ADJUST_LOCATION)
     uniform bool b_shadow_cascade_depth_adjust;
 layout(location = B_SHADOW_TEXEL_MOVE_LOCATION)
     uniform bool b_shadow_texel_move;
+layout(location = SHADOW_MODE_LOCATION)
+    uniform int shadow_mode;
 
 #endif  // SHADOW_SETTING_UNIFORMS_GLSL
